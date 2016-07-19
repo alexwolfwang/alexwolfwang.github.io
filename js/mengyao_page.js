e8c9551f0c87836df10ccf56eca2873af85f74b3
alexwolfwang <alexwolfwang@gmail.com>
2016-07-19 00:32:29 -0400

    JS files
    this is js part

---
+++ js/mengyao_page.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Alex.W on 2016/7/11.
+ */
+$(function() {
+    $('#mengyao').click(function() {
+        $(this).toggleClass('hover');
+    })
+    //$('[data-toggle="tooltip"]').tooltip('show');
+
+
+
+    $('.content span').click(function() {
+        $('#name_voice')[0].play();
+    });
+
+
+    $('body').on('click','.page_scroll a',function(event) {
+        var $anchor = $(this);
+        var $position = $($anchor.attr('href')).offset().top;
+        $('body').stop().animate({
+            scrollTop: $position
+        },1000,'swing');
+        event.preventDefault();
+    });
+
+
+});
+
+
+$(function() {
+    $("input,select,textarea").not("[type=submit]").jqBootstrapValidation();
+});
