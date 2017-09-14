17e25b758d36d2f14134b9fe6ddd058e01d8df2e
Alexwolfwang's Git <alexwolfwang@gmail.com>
2017-09-14 16:45:49 -0400

    2.0's js files

--- js/mengyao_contact.js
+++ js/mengyao_contact.js
@@ -19,94 +19,94 @@
     });
 
 
-$(function() {
-
-        $("#request_form input, #request_form textarea").jqBootstrapValidation({
-            preventSubmit:true,
-
-
-            submitSuccess:function($form, event){
-                // Prevent spam click and default submit behaviour
-                $(".btnSend").attr("disabled", true);
-                event.preventDefault();
-
-                var name = $('input.name').val().trim();
-                var email = $('input.email').val();
-                var msg = $('textarea.msg').val();
-                var Name = name;
-                var resumeRequest = $('#request_label')[0].checked;
-
-
-                if(Name.indexOf(' ') >= 0) {
-                    Name = Name.split(' ').slice(0).join(' ');
-                }
-
-
-                emailjs.send('mengyao','contact_msg',{"Name":Name,"email":email,"message":msg,"resume":resumeRequest}).then(function(response) {
-                    //msg send successful
-
-                    console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
-                    $('#btnSend').attr('disabled',false);
-                    $('#success_reply').html("<div class='alert alert-success'>");
-                    $('#success_reply > .alert-success').html("<strong>Thank you "+ Name +", your message has been sent</strong>").append("</div>");
-
-                    $('#request_form').each(function() {
-
-                        $('input').val('');
-                        $('textarea').val('');
-                    })
-                },function(err) {
-                    //msg send failure
-                    console.log("FAILED. error=",err);
-                    $('#btnSend').attr('disabled',true);
-                    $('#success_reply').html("<div class='alert alert-danger'>");
-                    $('#success_reply > .alert-danger').html("<strong><i class='fa fa-frown-o fa-2x'></i> Sorry " + Name +", the server is not responing. </strong>");
-                    $('#success_reply > .alert-danger').append("<br>");
-                    $('#success_reply > .alert-danger').append("<strong>Please try again later~~</strong>");
-                    $('#success_reply > .alert-danger').append("</div>")
-                })
-            }
-        });
-    //})
-});
-
-$(function() {
-    $(".message input, .message textarea").jqBootstrapValidation( {
-        preventSubmit: true,
-
-        submitSuccess: function($form, event) {
-            $(".msg_send").attr("disabled",true);
-            event.preventDefault();
-
-            var name = $('input.msgName').val().trim();
-            var email = $("input.msgEmail").val();
-            var msg = $('textarea.msgMessage').val();
-            var Name = name;
-            var resumeRequest = $('#request_label2')[0].checked;
-
-            if(Name.indexOf(' ') >= 0) {
-                Name = Name.split(' ').splice(0).join(' ');
-            }
-
-            emailjs.send('mengyao','contact_msg',{'Name':Name,'email':email, 'message':msg, 'resume':resumeRequest}).then(function(response) {
-                console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
-                $('.msg_send').attr("disabled",false);
-                $('.msg_reply').html("<div class='alert alert-success'>");
-                $(".msg_reply > .alert-success").html("<strong>Thank you "+ Name +", your message has been sent</strong>");
-
-                $(".message").each(function() {
-                    $('input').val('');
-                    $('textarea').val('');
-                })
-            },function(err) {
-                console.log("FAILED. error=",err);
-                $('.msg_reply').attr('disabled',true);
-                $('.msg_reply').html("<div class='alert alert-danger'>");
-                $('.msg_reply > .alert-danger').html("<strong><i class='fa fa-frown-o fa-2x'></i> Sorry " + Name +", the server is not responing. </strong>");
-                $('.msg_reply > .alert-danger').append("<br>");
-                $('#success_reply > .alert-danger').append("<strong>Please try again later~~</strong>");
-                $('#success_reply > .alert-danger').append("</div>")
-            })
-        }
-    })
-})
+// $(function() {
+//
+//         $("#request_form input, #request_form textarea").jqBootstrapValidation({
+//             preventSubmit:true,
+//
+//
+//             submitSuccess:function($form, event){
+//                 // Prevent spam click and default submit behaviour
+//                 $(".btnSend").attr("disabled", true);
+//                 event.preventDefault();
+//
+//                 var name = $('input.name').val().trim();
+//                 var email = $('input.email').val();
+//                 var msg = $('textarea.msg').val();
+//                 var Name = name;
+//                 var resumeRequest = $('#request_label')[0].checked;
+//
+//
+//                 if(Name.indexOf(' ') >= 0) {
+//                     Name = Name.split(' ').slice(0).join(' ');
+//                 }
+//
+//
+//                 emailjs.send('mengyao','contact_msg',{"Name":Name,"email":email,"message":msg,"resume":resumeRequest}).then(function(response) {
+//                     //msg send successful
+//
+//                     console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
+//                     $('#btnSend').attr('disabled',false);
+//                     $('#success_reply').html("<div class='alert alert-success'>");
+//                     $('#success_reply > .alert-success').html("<strong>Thank you "+ Name +", your message has been sent</strong>").append("</div>");
+//
+//                     $('#request_form').each(function() {
+//
+//                         $('input').val('');
+//                         $('textarea').val('');
+//                     })
+//                 },function(err) {
+//                     //msg send failure
+//                     console.log("FAILED. error=",err);
+//                     $('#btnSend').attr('disabled',true);
+//                     $('#success_reply').html("<div class='alert alert-danger'>");
+//                     $('#success_reply > .alert-danger').html("<strong><i class='fa fa-frown-o fa-2x'></i> Sorry " + Name +", the server is not responing. </strong>");
+//                     $('#success_reply > .alert-danger').append("<br>");
+//                     $('#success_reply > .alert-danger').append("<strong>Please try again later~~</strong>");
+//                     $('#success_reply > .alert-danger').append("</div>")
+//                 })
+//             }
+//         });
+//     //})
+// });
+//
+// $(function() {
+//     $(".message input, .message textarea").jqBootstrapValidation( {
+//         preventSubmit: true,
+//
+//         submitSuccess: function($form, event) {
+//             $(".msg_send").attr("disabled",true);
+//             event.preventDefault();
+//
+//             var name = $('input.msgName').val().trim();
+//             var email = $("input.msgEmail").val();
+//             var msg = $('textarea.msgMessage').val();
+//             var Name = name;
+//             var resumeRequest = $('#request_label2')[0].checked;
+//
+//             if(Name.indexOf(' ') >= 0) {
+//                 Name = Name.split(' ').splice(0).join(' ');
+//             }
+//
+//             emailjs.send('mengyao','contact_msg',{'Name':Name,'email':email, 'message':msg, 'resume':resumeRequest}).then(function(response) {
+//                 console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
+//                 $('.msg_send').attr("disabled",false);
+//                 $('.msg_reply').html("<div class='alert alert-success'>");
+//                 $(".msg_reply > .alert-success").html("<strong>Thank you "+ Name +", your message has been sent</strong>");
+//
+//                 $(".message").each(function() {
+//                     $('input').val('');
+//                     $('textarea').val('');
+//                 })
+//             },function(err) {
+//                 console.log("FAILED. error=",err);
+//                 $('.msg_reply').attr('disabled',true);
+//                 $('.msg_reply').html("<div class='alert alert-danger'>");
+//                 $('.msg_reply > .alert-danger').html("<strong><i class='fa fa-frown-o fa-2x'></i> Sorry " + Name +", the server is not responing. </strong>");
+//                 $('.msg_reply > .alert-danger').append("<br>");
+//                 $('#success_reply > .alert-danger').append("<strong>Please try again later~~</strong>");
+//                 $('#success_reply > .alert-danger').append("</div>")
+//             })
+//         }
+//     })
+// })
--- js/mengyao_page.js
+++ js/mengyao_page.js
@@ -2,7 +2,75 @@
  * Created by Alex.W on 2016/7/11.
  */
 $(function() {
-    $('#mengyao').click(function() {
+    $('#fullpage').fullpage({
+        //Navigation
+        menu: '#menu',
+        anchors:['Mengyao','about', 'portfolio', 'project', 'contact'],
+        navigation: true,
+        navigationPosition: 'right',
+        // navigationTooltips: ['首页', '国际汇款','基金管理','证劵业务','方圆支付','创业孵化园','联系我们'],
+        showActiveTooltip: false,
+        slidesNavigation: false,
+        slidesNavPosition: 'bottom',
+        // paddingTop: '90px',
+        //Scrolling
+        css3: true,
+        scrollingSpeed: 700,
+        autoScrolling: true,
+        fitToSection: true,
+        fitToSectionDelay: 1000,
+        scrollBar: false,
+        easing: 'easeInOutCubic',
+        easingcss3: 'ease',
+        loopBottom: false,
+        loopTop: false,
+        loopHorizontal: true,
+        continuousVertical: false,
+        continuousHorizontal: false,
+        scrollHorizontally: false,
+        interlockedSlides: false,
+        dragAndMove: false,
+        offsetSections: false,
+        resetSliders: false,
+        fadingEffect: false,
+        normalScrollElements: '#element1, .element2',
+        scrollOverflow: false,
+        scrollOverflowReset: false,
+        scrollOverflowOptions: null,
+        touchSensitivity: 15,
+        normalScrollElementTouchThreshold: 5,
+        bigSectionsDestination: null,
+
+        //Accessibility
+        keyboardScrolling: true,
+        animateAnchor: true,
+        recordHistory: true,
+
+        //Design
+        controlArrows: true,
+        verticalCentered: true,
+
+        fixedElements: '#header, .footer',
+        responsiveWidth: 0,
+        responsiveHeight: 0,
+        responsiveSlides: false,
+
+        //Custom selectors
+        sectionSelector: '.section',
+
+        lazyLoading: true,
+
+        //events
+        onLeave: function(index, nextIndex, direction){},
+        afterLoad: function(anchorLink, index){},
+        afterRender: function(){},
+        afterResize: function(){},
+        afterResponsive: function(isResponsive){},
+        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+        onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
+    });
+
+    $('.mengyao').click(function() {
         $(this).toggleClass('hover');
     });
     //$('[data-toggle="tooltip"]').tooltip('show');
@@ -14,16 +82,75 @@ $(function() {
     });
 
 
-    $('body').on('click','.page_scroll a',function(event) {
-        var $anchor = $(this);
-        var $position = $($anchor.attr('href')).offset().top;
-        $('body').stop().animate({
-            scrollTop: $position
-        },1000,'swing');
-        event.preventDefault();
-    });
+
+
+
 
     $('body').scrollspy({target:'.navbar',offset:80})
+
+
+    $('.contact .submit1, .contact .submit2').click(function () {
+
+        var name = $(this).parents('form').find('.name').val(), email = $(this).parents('form').find('.email').val(),
+            phone = $(this).parents('form').find('.phone').val(), consult = $(this).parents('form').find('.consult').val();
+        var emailReg = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+        var phoneReg = /^[1-9][0-9]{9}$/;
+        if(name == '' || phone == '' || email == '' || consult == '' || phoneReg.test(phone) == false || emailReg.test(email) == false ) {
+            if(name == '') {
+                $(this).parents('form').find(' .name ~ .error-label').removeClass('hide');
+            }
+            if(consult == '') {
+                $(this).parents('form').find(' .consult ~ .error-label').removeClass('hide');
+            }
+            if(phone == '' || phoneReg.test(phone) == false) {
+                $(this).parents('form').find(' .phone ~ .error-label').removeClass('hide');
+            }
+            if(email == '' || emailReg.test(email) == false) {
+                $(this).parents('form').find(' .email ~ .error-label').removeClass('hide');
+            }
+        } else {
+            // alert("ok");
+            var resumeRequest = $('#request_label2')[0].checked;
+            sendEmail(name,email,phone,consult,resumeRequest);
+            $(this).parents('form').find('.name').val('');
+            $(this).parents('form').find('.email').val('');
+            $(this).parents('form').find('.phone').val('');
+            $(this).parents('form').find('.consult').val('');
+            $(".subBtn").addClass( "onclic", 250, validate());
+
+        }
+    });
+
+    $('.contactInput .name, .contactInput textarea').blur(function () {
+        if($(this).val() != '') {
+            $(this).siblings('.error-label').addClass('hide');
+        }
+    });
+
+    $('.phone').blur(function () {
+        var phoneReg = /^[1-9][0-9]{9}$/;
+        if(phoneReg.test($(this).val()) == true) {
+            $(this).siblings('.error-label').addClass('hide');
+        }
+    });
+    $('.email').blur(function () {
+        var emailReg = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+        if(emailReg.test($(this).val()) == true) {
+            $(this).siblings('.error-label').addClass('hide');
+        }
+    });
+
+    function validate() {
+        setTimeout(function() {
+            $( ".subBtn" ).removeClass( "onclic" );
+            $( ".subBtn" ).addClass( "validate", 450, callback() );
+        }, 2250 );
+    }
+    function callback() {
+        setTimeout(function() {
+            $( ".subBtn" ).removeClass( "validate" );
+        }, 2250 );
+    }
 
 });
 
@@ -31,3 +158,18 @@ $(function() {
 $(function() {
     $("input,select,textarea").not("[type=submit]").jqBootstrapValidation();
 });
+
+function sendEmail(n,em,p,cons,resume) {
+    emailjs.send('mengyao','contact_msg',{
+        name: n,
+        phone: p,
+        email: em,
+        message: cons,
+        'resume':resume
+    })
+        .then(function(response) {
+            console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
+        }, function(err) {
+            console.log("FAILED. error=", err);
+        });
+}
